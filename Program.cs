165b76f1a10ffcd018e1c01b4b6c42098896c6ba
Vladislav <bayanovlad@gmail.com>
2023-09-30 14:20:19 +0300

    Сделал все 3 задачи

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+// int num = new Random().Next(100,1000);
+// Console.WriteLine(num);
+// int num1 = num/100;
+// int num2 = num%10;
+
+// int result = num1*10+num2;
+
+// Console.WriteLine(result);
+
+
+ 
+
+
+// if (num1>num2)
+// {
+//     Console.WriteLine(num1);
+// }
+// else
+// {
+//     Console.WriteLine(num2);
+// }
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1%num2==0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Не кратно" + num1%num2+ "s"); // Канкатинация
+    Console.WriteLine($"{num1}Не кратно  {num1%num2} s"); // Интерполяция
+}
